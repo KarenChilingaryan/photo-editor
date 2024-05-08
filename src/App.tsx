c46ce96a50201b1c7c77face07cd812e98491b8c
KarenChilingaryan <karen.chilingaryann98@gmail.com>
2024-05-09 03:26:26 +0400

    fixed

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ColorDropper from './Canvas';
+
+function App() {
+  return (
+    <div className="App">
+      <ColorDropper />
+    </div>
+  );
+}
+
+export default App;
+// const imageUrl = '/images/1920x1080-4598441-beach-water-pier-tropical-sky-sea-clouds-island-palm-trees.jpg'; // Replace with your image URL
